05573b7f7581649ebb1b58964f3361a40766aa48
Conor <conor.manning1996@gmail.com>
2019-07-17 17:41:11 -0400

    Created a struct to keep track of the user's preferences

---
+++ GoBoken/Helpers/UserFilterPreferences.swift
@@ -0,0 +1,16 @@
+//
+//  UserFilterPreferences.swift
+//  GoBoken
+//
+//  Created by Conor Manning on 7/17/19.
+//  Copyright © 2019 GoBoken. All rights reserved.
+//
+
+import Foundation
+
+struct UserFilterPreferences {
+  
+  static var maximumDistance: Float = 2.0
+  static var chosenKeywords = LocationKeywords.allCases
+  
+}
