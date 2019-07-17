49a242c0e2a0ab5742a3858151d017fd8350b2a0
Conor <conor.manning1996@gmail.com>
2019-07-17 11:04:54 -0400

    Added an enum for keyword cases

---
+++ GoBoken/Helpers/LocationKeywords.swift
@@ -0,0 +1,16 @@
+//
+//  LocationKeywords.swift
+//  GoBoken
+//
+//  Created by Conor Manning on 7/17/19.
+//  Copyright © 2019 GoBoken. All rights reserved.
+//
+
+import Foundation
+
+enum LocationKeywords: CaseIterable {
+  case dining
+  case hotel
+  case landmark
+  case coffee
+}
