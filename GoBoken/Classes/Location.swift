706c0f44381a06219e619be5a840129992dd48d7
Conor <conor.manning1996@gmail.com>
2019-07-17 10:29:49 -0400

    Added coordinates as a property of Location

--- GoBoken/Classes/Location.swift
+++ GoBoken/Classes/Location.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreLocation
 
-struct Location: Hashable {
+class Location: Hashable {
   
   var name: String!
   var address: String!
@@ -21,10 +21,30 @@ struct Location: Hashable {
   
   init(name: String, address: String, image: UIImage, keywords: [String], description: String) {
     self.name = name
-    self.address = address
+    self.address = address + ", Hoboken, NJ 07030"
     self.image = image
     self.keywords = keywords
     self.description = description
+    setAddress()
+  }
+  
+  func setAddress() -> Void {
+    let geoCoder = CLGeocoder()
+    geoCoder.geocodeAddressString(address) { (placemarks, error) in
+      guard let placemarks = placemarks, let coords = placemarks.first?.location else {
+        print("Could not get coords for " + self.address)
+        return
+      }
+      self.coordindates = coords
+    }
+  }
+  
+  static func == (lhs: Location, rhs: Location) -> Bool {
+    return lhs.name == rhs.name
+  }
+  
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(name)
   }
   
 }
